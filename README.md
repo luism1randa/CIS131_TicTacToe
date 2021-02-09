2875adf12714b77fca53d54307fec5f3d2722771
Luis Miranda <61203868+luism1randa@users.noreply.github.com>
2021-02-09 15:56:46 -0700

    Created project files

--- README.md
+++
@@ -1,4 +0,0 @@
-# CIS131_TicTacToe
-Tic-Tac-Toe game to played from the command line
-
-Game written for CIS 131 Course
---
+++ src/TicTacToe.java
@@ -0,0 +1,12 @@
+/**
+ *
+ *  @author Luis Miranda
+ *  @version 1
+ *
+ */
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
